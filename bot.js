e7fffb1a882f34220889d526b49c1f13f41e89a5
hamid814 <hamid331994@gmail.com>
2020-09-04 15:35:02 +0430

    dev

--- bot.js
+++ bot.js
@@ -1,26 +1,26 @@
 const runBot = (Telegraf, token) => {
-  // const bot = new Telegraf(token);
+  const bot = new Telegraf(token);
 
-  // bot.start((ctx) => ctx.reply('hehe, from code'));
+  bot.start((ctx) => ctx.reply('hehe, from code'));
 
-  // bot.command('sendall', (ctx) => ctx.reply('چشم'));
-  // bot.command('sayname', (ctx) => ctx.reply('hamid'));
+  bot.command('sendall', (ctx) => ctx.reply('چشم'));
+  bot.command('sayname', (ctx) => ctx.reply('hamid'));
 
-  // bot.on('text', (ctx) => {
-  //   console.log('on text');
+  bot.on('text', (ctx) => {
+    console.log('on text');
 
-  //   bot.telegram.sendMessage('hehe test');
+    bot.telegram.sendMessage('hehe test');
 
-  //   if (ctx.message.text === 'tell me') {
-  //     ctx.reply(`im telling you`);
+    if (ctx.message.text === 'tell me') {
+      ctx.reply(`im telling you`);
 
-  //     console.log('i told him');
-  //   } else {
-  //     ctx.reply(`:: ${ctx.message.from.username} ::`);
-  //   }
-  // });
+      console.log('i told him');
+    } else {
+      ctx.reply(`:: ${ctx.message.from.username} ::`);
+    }
+  });
 
-  // bot.launch();
+  bot.launch();
 
   console.log('bot v5');
 };
