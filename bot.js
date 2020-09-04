74d4248725f6bd9d428eda73de2955bc2f4db33c
hamid814 <hamid331994@gmail.com>
2020-09-04 15:58:51 +0430

    dev

--- bot.js
+++ bot.js
@@ -1,28 +1,73 @@
 const runBot = (Telegraf, token) => {
   const bot = new Telegraf(token);
 
-  bot.start((ctx) => ctx.reply('hehe, from code'));
+  let textState = null;
 
-  bot.command('sendall', (ctx) => ctx.reply('چشم'));
-  bot.command('sayname', (ctx) => ctx.reply('hamid'));
+  const data = {
+    origin: '',
+    destination: '',
+  };
+
+  const setTextState = (state) => {
+    textState = state;
+  };
+
+  bot.start((ctx) => ctx.reply('bot is being developed'));
+
+  bot.command('sendall', (ctx) => {
+    setTextState('gettingOriginChannel');
+
+    ctx.reply('enter name of origin channel');
+  });
+
+  bot.command('getdata', (ctx) => {
+    ctx.reply(`
+    origin: ${data.origin}
+    destination: ${data.destination}
+    `);
+  });
+
+  bot.command('cleardata', (ctx) => {
+    data.origin = '';
+    data.destination = '';
+
+    ctx.reply('cleared');
+  });
+
+  bot.command('cancel', (ctx) => {
+    setTextState(null);
+
+    ctx.reply(`select a command`);
+  });
 
   bot.on('text', (ctx) => {
-    console.log('on text');
+    if (textState === 'gettingOriginChannel') {
+      data.origin = ctx.message.text;
 
-    bot.telegram.sendMessage('hehe test');
+      setTextState('gettingDestChannel');
+
+      ctx.reply('enter name of destination channel');
+    }
+
+    if (textState === 'gettingDestChannel') {
+      data.destination = ctx.message.text;
+
+      setTextState(null);
+      ctx.reply('Done for now');
+    }
 
     if (ctx.message.text === 'tell me') {
       ctx.reply(`im telling you`);
 
       console.log('i told him');
     } else {
-      ctx.reply(`:: ${ctx.message.from.username} ::`);
+      ctx.reply(`select a command`);
     }
   });
 
   bot.launch();
 
-  console.log('bot v5');
+  console.log('bot v1');
 };
 
 module.exports = runBot;
